b9e2f84b2aac2263b6fb1ada8a4145cf7a6a5426
unknown <talktogbenga@gmail.com>
2024-07-09 23:55:37 +0100

    Building App for Heroku

--- migrations/20240708112000_add_table_wallet.ts
+++ migrations/20240708112000_add_table_wallet.ts
@@ -5,7 +5,11 @@ export async function up(knex: Knex): Promise<void> {
     table.increments("id").primary();
     table.uuid("uuid").defaultTo(knex.raw("(UUID())")).notNullable().unique();
     table.integer("user_id").unsigned().notNullable();
-    table.foreign("user_id").references("id").inTable("users"); // Assuming the referenced table is 'users'
+    table
+      .foreign("user_id")
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE"); // Assuming the referenced table is 'users'
     table.double("amount", 14, 2).notNullable().defaultTo(0);
     table.timestamps(true, true);
   });
--- migrations/20240709081430_create_transactions_table.ts
+++ migrations/20240709081430_create_transactions_table.ts
@@ -4,8 +4,9 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("transactions", function (table) {
     table.increments("id").primary();
     table.uuid("uuid").defaultTo(knex.raw("(UUID())")).notNullable().unique();
-    table.integer("payer-id").notNullable();
-    table.integer("payee-id");
+    table.integer("payer_id").notNullable();
+    table.foreign("payer_id").references("id").inTable("users");
+    table.integer("payee_id");
     table.double("amount", 14, 2).notNullable().defaultTo(0);
     table.boolean("settled").notNullable().defaultTo(false);
     table.text("description").notNullable();
